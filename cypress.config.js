47b4b4b75488ccd97fecf2aa36def26ec530b919
João Vitor Gomes Bastos dos Santos <bgomes.joaovitorads@gmail.com>
2025-07-31 20:51:58 -0300

    test: att cypress config.

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,12 @@ const { defineConfig } = require("cypress");
 const fs = require('fs');
 
 module.exports = defineConfig({
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  defaultCommandTimeout: 10000,
   e2e: {
     setupNodeEvents(on, config) {
       on('task', {
-
       });
     },
   },
